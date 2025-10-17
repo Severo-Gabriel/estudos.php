72bbb754f268cae81154026d270ed05a2e228263
Gabriel Severo <severogsp@gmail.com>
2025-10-17 10:47:53 -0300

    Create dados.php

---
+++ Estudos/dados.php
@@ -0,0 +1,10 @@
+<?php 
+    // Tipos primitivos: double, string, float, bool, int;
+
+    $conta = 5+5;
+    $nome = "Gabriel";
+    $estadocivil = false; 
+    $peso = 76.2;
+
+    var_dump($estadocivil, $peso, $conta, $nome)
+    ?>
