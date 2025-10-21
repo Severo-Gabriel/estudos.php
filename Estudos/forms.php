0c5426573f9a12648f07860352b1f6302eef8985
Gabriel Severo <severogsp@gmail.com>
2025-10-21 11:16:56 -0300

    Update forms.php

--- Estudos/forms.php
+++ Estudos/forms.php
@@ -3,4 +3,4 @@
         $sobrenome = $_REQUEST["sobrenome"] ?? "desconhecido";
         echo "É um prazer te conhecer, <strong>$nome <strong>$sobrenome!";
         ?>
-        <p><a href="Javascript:history.go(-1)">Voltar para a página anterior</a></p>
+      
