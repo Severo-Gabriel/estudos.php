5908ccd015ffe479be65a32e70c6d1fdd66e1667
Gabriel Severo <severogsp@gmail.com>
2025-10-22 12:21:38 -0300

    Add cash withdrawal calculation in caixa_eletronico.php
    Implement cash withdrawal logic for various denominations.

--- Desafios/caixa_eletronico.php
+++ Desafios/caixa_eletronico.php
@@ -1,1 +1,22 @@
+ <?php 
+        // Valor que a pessoa quer sacar
+        $valor = 1550;
+        $total = $valor;
 
+        $val100 = (int)($total / 100);
+        $total = $total % 100;
+
+        $valor50 = (int)($total / 50);
+        $total = $total % 50;
+
+        $valor20 = (int)($total / 20);
+        $total = $total % 20;
+
+        $valor10 = (int)($total / 10);
+        $total = $total % 10;
+
+        $valor5 = (int)($total / 5);
+        $total = $total % 5;
+
+        echo "O saque de R$ $valor vai ser de: $val100 cédulas de 100. $valor50 cédulas de 50, $valor20 cédulas de 20, $valor10 cédulas de 10 e $valor5 cédulas de 5. Falta sacar $total."
+        ?>
